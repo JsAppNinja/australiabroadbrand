f257b390b92f49c24369e7500fb34fc1472762d1
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-20 10:34:44 +0000

    eslint add, err fix

--- src/app/components/AppLink/AppLink.js
+++ src/app/components/AppLink/AppLink.js
@@ -1,14 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const AppLink = ({ children, ...rest }) => <Link {...rest}>{children}</Link>;
 
 AppLink.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
 AppLink.defaultProps = {
-  children: null
+  children: null,
 };
 
 export default AppLink;
--- src/app/containers/home/index.js
+++ src/app/containers/home/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Page from "../../components/Page";
-import HeroSection from "./HeroSection";
+import React from 'react';
+import Page from '../../components/Page';
+import HeroSection from './HeroSection';
 
 export default () => (
   <Page id="homepage">
