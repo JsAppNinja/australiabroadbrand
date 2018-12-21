241053ac1aec7d4ae3102d6a0cbf85368bee14de
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-21 12:15:26 +0000

    Information section add, Roboto font import

---
+++ src/app/containers/home/ImpInformation/ImpInformation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const ImpInformation = () => (
+  <div className="imp-information">
+    Important information
+    <a>
+      <i className="fa fa-chevron-down" />
+    </a>
+  </div>
+);
+
+export default ImpInformation;
--- src/app/containers/home/index.js
+++ src/app/containers/home/index.js
@@ -3,11 +3,13 @@ import Page from '../../components/Page';
 import HeroSection from './HeroSection';
 import BenefitSection from './BenefitSection';
 import CheckAddress from './CheckAddress';
+import Information from './ImpInformation';
 
 export default () => (
   <Page id="homepage">
     <HeroSection />
     <BenefitSection />
     <CheckAddress />
+    <Information />
   </Page>
 );
