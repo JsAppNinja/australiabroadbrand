35f15eb16a5073bef3f942a114c167e270aff0c7
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-23 12:53:44 +0000

    ReviewSection created

---
+++ src/app/containers/home/ReviewSection/ReviewSection.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './style.scss';
+
+class ReviewSection extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="review-section" />;
+  }
+}
+
+export default ReviewSection;
