8a020bada6cba8e27a98d28faaf32200d81cbdda
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-24 00:19:10 +0000

    add ReviewItem, global styles, variables

--- src/app/components/ReviewItem/ReviewItem.js
+++ src/app/components/ReviewItem/ReviewItem.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import StarRating from 'components/StarRating';
 
-import './style.scss';
+const ReviewItem = ({ rating, date, description, title, name, featured }) => {
+  const className = cx('kreview', {
+    'kreview--featured': featured,
+  });
 
-class ReviewItem extends Component {
-  constructor(props) {
-    super(props);
-  }
+  return (
+    <div className={className}>
+      <div className="kreview__rating">
+        <StarRating rating={rating} />
+      </div>
+      <div className="kreview__content">
+        {title && <div className="kreview__content-title">“{title}”</div>}
+        <div className="kreview__content-reviewer">
+          <div className="kreview__content-reviewer__name">
+            {name || 'Anonymous'}
+          </div>
+          <div className="kreview__content-reviewer__date">{date}</div>
+        </div>
+        <div className="kreview__content-description">{description}</div>
+      </div>
+    </div>
+  );
+};
 
-  render() {
-    return <div className="review-item" />;
-  }
-}
+ReviewItem.propTypes = {
+  featured: PropTypes.bool,
+  rating: PropTypes.string,
+  name: PropTypes.string,
+  date: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
+
+ReviewItem.defaultProps = {
+  featured: false,
+  rating: 5,
+};
 
 export default ReviewItem;
