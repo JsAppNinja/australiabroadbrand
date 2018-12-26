0a2209d19252b51d115833e2ee19baa4b684143e
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-26 10:42:48 +0000

    create footer component, add assets

---
+++ src/app/containers/home/footer/footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import DATA from './DATA';
+import './style.scss';
+
+import LOREM from '../../../assets/svgs/lorem.svg';
+import FAV_NBN from '../../../assets/footer-nbn.png';
+import FAV_EWAY from '../../../assets/footer-eway.png';
+import FAV_GOOGLE_PLAY from '../../../assets/footer-gplay.png';
+import FAV_APPSTORE from '../../../assets/footer-appstore.png';
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="footer" />;
+  }
+}
+
+export default Footer;
