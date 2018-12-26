7cf6315bc12aecd714cddd27bbdeb9893d368141
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-26 02:22:47 +0000

    update ReviewItem component, add assets

--- src/app/components/ReviewItem/ReviewItem.js
+++ src/app/components/ReviewItem/ReviewItem.js
@@ -3,31 +3,34 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import StarRating from '../StarRating';
 
-const ReviewItem = ({ rating, date, description, title, name, featured }) => {
+const ReviewItem = ({ rating, userCount, reviewImg }) => {
   const className = cx('kreview', {
     'kreview--featured': featured,
   });
 
   return (
     <div className={className}>
-      <div className="kreview__rating">
-        <StarRating rating={rating} />
+      <div className="kreview-score">
+        <div className="kreview-score__mark">{rating}</div>
+        <div className="kreview__rating">
+          <div className="kreview__rating-count">from {userCount} reviews</div>
+          <StarRating rating={rating} />
+        </div>
+      </div>
+      <div className="kreview-reviewImage">
+        <img src={reviewImg} />
       </div>
     </div>
   );
 };
 
 ReviewItem.propTypes = {
-  featured: PropTypes.bool,
-  rating: PropTypes.string,
-  name: PropTypes.string,
-  date: PropTypes.string,
-  title: PropTypes.string,
-  description: PropTypes.string,
+  rating: PropTypes.number,
+  userCount: PropTypes.string,
+  reviewImg: PropTypes.string,
 };
 
 ReviewItem.defaultProps = {
-  featured: false,
   rating: 5,
 };
 
