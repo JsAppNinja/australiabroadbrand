96741e4271268664b0ae768e2850f394af2de12b
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-28 10:22:11 +0000

    create CircleLink component

--- src/app/components/CircleLink/CircleLink.js
+++ src/app/components/CircleLink/CircleLink.js
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import ExCircle from '../ExCircle';
 import './style.scss';
 
-const CircleLink = ({ ImgPaths }) => <div className="circle-link">Test</div>;
+const CircleLink = ({ ImgPaths }) => (
+  <div className="circle-link">
+    {ImgPaths.map((item, index) => (
+      <div key={index} className="circle-link__one-circle">
+        <ExCircle BgImgPath={item} ChildContent={index + 1} />
+      </div>
+    ))}
+  </div>
+);
 
 CircleLink.propTypes = {
   ImgPaths: PropTypes.array,
--- src/app/containers/home/EasyConnection/EasyConnection.js
+++ src/app/containers/home/EasyConnection/EasyConnection.js
@@ -1,11 +1,18 @@
 import React from 'react';
-// import ExCircle from '../../../components/ExCircle';
+import CircleLink from '../../../components/CircleLink';
+
 import './style.scss';
 import FavHome from '../../../assets/svgs/connect-home.svg';
 import FavUser from '../../../assets/svgs/connect-user.svg';
 import FavNbn from '../../../assets/svgs/connect-nbn.png';
 import FavEnjoy from '../../../assets/svgs/connect-enjoy.svg';
 
-const EasyConnection = () => <div className="easy-connection" />;
+const favIconList = [FavHome, FavUser, FavNbn, FavEnjoy];
+
+const EasyConnection = () => (
+  <div className="easy-connection">
+    <CircleLink ImgPaths={favIconList} />
+  </div>
+);
 
 export default EasyConnection;
