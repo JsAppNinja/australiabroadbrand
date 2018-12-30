1830c06c6c998842e55548a60d245808cacc350e
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2018-12-30 11:33:29 +0000

    Easy Connection Bg update

--- src/app/containers/home/EasyConnection/EasyConnection.js
+++ src/app/containers/home/EasyConnection/EasyConnection.js
@@ -6,11 +6,15 @@ import FavHome from '../../../assets/svgs/connect-home.svg';
 import FavUser from '../../../assets/svgs/connect-user.svg';
 import FavNbn from '../../../assets/svgs/connect-nbn.png';
 import FavEnjoy from '../../../assets/svgs/connect-enjoy.svg';
+import EasyBg from '../../../assets/easy-connection-bg.png';
 
 const favIconList = [FavHome, FavUser, FavNbn, FavEnjoy];
 
 const EasyConnection = () => (
-  <div className="easy-connection">
+  <div
+    className="easy-connection"
+    style={{ backgroundImage: `url('${EasyBg}')` }}
+  >
     <CircleLink ImgPaths={favIconList} />
   </div>
 );
