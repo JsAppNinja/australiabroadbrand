e202ec2fa514f67a9676471877d484036c0d0a36
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-04 00:44:15 +0000

    adding the path lines

--- src/app/containers/home/EasyConnection/EasyConnection.js
+++ src/app/containers/home/EasyConnection/EasyConnection.js
@@ -11,28 +11,9 @@ import EasyBg from '../../../assets/easy-connection-bg.png';
 const favIconList = [FavHome, FavUser, FavNbn, FavEnjoy];
 
 const EasyConnection = () => {
-  const path = (
-    <path
-      d={`
-        M 25,25
-        C 100,50 25,75 25,100
-        C 25,125 300,150 25,175
-      `}
-      fill="none"
-      stroke="hotpink"
-      strokeWidth={5}
-    />
-  );
-
   return (
     <div className="easy-connection">
       <CircleLink ImgPaths={favIconList} />
-      {/* <svg
-        viewBox="0 0 200 200"
-        style={{ maxHeight: 400 }}
-      >
-        {path}
-      </svg> */}
     </div>
   );
 };
