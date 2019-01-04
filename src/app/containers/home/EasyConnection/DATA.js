dcd7bd7a544e6673be14957d69cea3080d33b75f
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-04 02:49:47 +0000

    Easy Connection data part attach

---
+++ src/app/containers/home/EasyConnection/DATA.js
@@ -0,0 +1,20 @@
+export default {
+  desc: [
+    {
+      title: 'Check your address',
+      content: `Enter your address and see if you can nbn™ at your address. If you address is nbn™ ready, choose a plan, enter your details and sign up online with ease.`,
+    },
+    {
+      title: 'Get connected',
+      content: `Setup your modem, get online and enjoy stress free streaming & fast internet on the nbn™`,
+    },
+    {
+      title: 'Sign up online',
+      content: `Sit back & relax as we'll look after the nbn™ arrangements for you. If an NBN Co technician is required we’ll send one out to get you setup`,
+    },
+    {
+      title: 'Sit back & enjoy',
+      content: `Sit back & relax as we'll look after the nbn™ arrangements for you. If an NBN Co technician is required we’ll send one out to get you setup`,
+    },
+  ],
+};
--- src/app/containers/home/EasyConnection/EasyConnection.js
+++ src/app/containers/home/EasyConnection/EasyConnection.js
@@ -6,14 +6,39 @@ import FavHome from '../../../assets/svgs/connect-home.svg';
 import FavUser from '../../../assets/svgs/connect-user.svg';
 import FavNbn from '../../../assets/svgs/connect-nbn.png';
 import FavEnjoy from '../../../assets/svgs/connect-enjoy.svg';
-import EasyBg from '../../../assets/easy-connection-bg.png';
 
+import Data from './DATA';
 const favIconList = [FavHome, FavUser, FavNbn, FavEnjoy];
 
 const EasyConnection = () => {
   return (
     <div className="easy-connection">
+      <div className="easy-connection__left-section">
+        <div className="easy-connection__left-section-top">
+          <div className="description-curveline" />
+          <div className="section-description">
+            <div className="section-description__title">
+              {Data.desc[0].title}
+            </div>
+            <div className="section-description__content">
+              {Data.desc[0].content}
+            </div>
+          </div>
+        </div>
+        <div className="easy-connection__left-section-down">
+          <div className="description-curveline" />
+          <div className="section-description">
+            <div className="section-description__title">
+              {Data.desc[1].title}
+            </div>
+            <div className="section-description__content">
+              {Data.desc[1].content}
+            </div>
+          </div>
+        </div>
+      </div>
       <CircleLink ImgPaths={favIconList} />
+      <div className="easy-connection__right-section"> Right Section</div>
     </div>
   );
 };
