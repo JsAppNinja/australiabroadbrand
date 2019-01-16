9788f6d2bce86b4a043a11ce1d7c4e95b61fe0bb
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-16 10:43:06 +0000

    Nbn Types Section UI update

--- src/app/containers/nbn-explained/NbnTypes/Description.js
+++ src/app/containers/nbn-explained/NbnTypes/Description.js
@@ -1,40 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import './style.scss';
 
-const Description = (title, content, link, number, colors) => (
-  <div className="type-description">
-    <div
-      className="type-description__main"
-      style={{
-        backgroundColor: colors[0],
-      }}
-    >
-      <div className="type-description__main-title">{title}</div>
-      <div className="type=description__main-content">{content}</div>
-      <Link
-        to={link}
-        style={{
-          fontColor: colors[1],
-        }}
-      >
-        more info -->
-      </Link>
-    </div>
-    <div className="type-description__number">
-      <div
-        className="type-description__number-content"
-        style={{
-          backgroundColor: colors[2],
-        }}
-      >
-        {number}
+class Description extends Component {
+  render() {
+    const { title, content, link, number, colors } = this.props;
+    return (
+      <div className="type-description">
+        <div
+          className="type-description__main"
+          style={{
+            backgroundColor: colors[0],
+          }}
+        >
+          <div className="type-description__main-title">{title}</div>
+          <div className="type=description__main-content">{content}</div>
+          <Link
+            to={link}
+            style={{
+              fontColor: colors[1],
+            }}
+          >
+            more info -->
+          </Link>
+        </div>
+        <div className="type-description__number">
+          <div
+            className="type-description__number-content"
+            style={{
+              backgroundColor: colors[2],
+            }}
+          >
+            {number}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 Description.propTypes = {
   title: PropTypes.string,
--- src/app/containers/nbn-explained/index.js
+++ src/app/containers/nbn-explained/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Page from '../../components/Page';
 import NbnConcept from './NbnConcept';
+import NbnTypes from './NbnTypes';
 import Footer from '../footer';
-
 import './style.scss';
 
 export default () => (
   <Page id="nbn-explained">
     <NbnConcept />
+    <NbnTypes />
   </Page>
 );
