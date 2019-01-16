eb766f625f2e34c83ed6d0124ed07aa793886b1f
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-16 16:47:28 +0000

    UI develop of NBNHelp

--- src/app/containers/nbn-explained/NbnHelp/NbnHelp.js
+++ src/app/containers/nbn-explained/NbnHelp/NbnHelp.js
@@ -5,6 +5,10 @@ import NBN_ICON1 from '../../../assets/svgs/nbn-help2.svg';
 import NBN_ICON2 from '../../../assets/svgs/nbn-help3.svg';
 import NBN_ICON3 from '../../../assets/svgs/nbn-help4.svg';
 import NBN_ICON4 from '../../../assets/svgs/nbn-help5.svg';
+
+import NBN_HELP_PHONE from '../../../assets/svgs/nbn-help-phone.svg';
+import NBN_HELP_EMAIL from '../../../assets/svgs/nbn-help-email.svg';
+
 import './style.scss';
 
 const NbnHelp = () => <div className="nbn-help">Important information</div>;
