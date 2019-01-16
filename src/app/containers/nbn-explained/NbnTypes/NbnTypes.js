1fc9a9609469eff65246d99505720fcca054a237
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-16 03:01:05 +0000

    add Nbn Types assets, ui update

--- src/app/containers/nbn-explained/NbnTypes/NbnTypes.js
+++ src/app/containers/nbn-explained/NbnTypes/NbnTypes.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 import Description from './Description';
+import Data from './Data';
+
+import EXPLAIN_IMG1 from '../../../assets/nbn-types1.png';
+import EXPLAIN_IMG2 from '../../../assets/nbn-types2.png';
+import EXPLAIN_IMG3 from '../../../assets/nbn-types3.png';
+import EXPLAIN_IMG4 from '../../../assets/nbn-types4.png';
+import EXPLAIN_IMG5 from '../../../assets/nbn-types5.png';
+import EXPLAIN_IMG6 from '../../../assets/nbn-types6.png';
 import './style.scss';
 
 const NbnTypes = () => <div className="nbntypes" />;
