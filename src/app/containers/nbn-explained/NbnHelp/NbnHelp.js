90cb97b7ad4d66e0df2a13639e3e5abd4132341c
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-16 16:55:14 +0000

    NBNHelp UI update

--- src/app/containers/nbn-explained/NbnHelp/NbnHelp.js
+++ src/app/containers/nbn-explained/NbnHelp/NbnHelp.js
@@ -11,6 +11,36 @@ import NBN_HELP_EMAIL from '../../../assets/svgs/nbn-help-email.svg';
 
 import './style.scss';
 
-const NbnHelp = () => <div className="nbn-help">Important information</div>;
+const NbnHelp = () => (
+  <div className="nbn-help">
+    <div className="nbn-help__left">
+      <div className="nbn-help__left-title">WE'RE HERE TO HELP</div>
+      <div className="nbn-help__left-content">
+        We're open 9am to Midnight, Monday to Friday and 9am to 6pm
+        <br />
+        Weekends and Public Hoildays.
+      </div>
+      <div className="nbn-help__left-phone">
+        <img src={NBN_HELP_PHONE} alt="lprem" />
+        Call 1300 023 354 and talk to us.
+      </div>
+      <div className="nbn-help__left-email">
+        <img src={NBN_HELP_EMAIL} alt="lprem" />
+        Email Us 24/7. We'll make sure you're quickly put in
+        <br />
+        touch with someone who’s ready to help.
+      </div>
+      <div className="nbn-help__left-favicon">
+        <img src={NBN_ICON1} alt="lprem" />
+        <img src={NBN_ICON2} alt="lprem" />
+        <img src={NBN_ICON3} alt="lprem" />
+        <img src={NBN_ICON4} alt="lprem" />
+      </div>
+    </div>
+    <div className="nbn-help-right">
+      <img src={NBN_HELP} alt="lprem" />
+    </div>
+  </div>
+);
 
 export default NbnHelp;
