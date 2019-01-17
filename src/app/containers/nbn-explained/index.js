933657f884da35b7edf8e8ae3ea699ecf1a837cb
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-17 02:24:08 +0000

    import the Install Secinto into NBN Explained

--- src/app/containers/nbn-explained/index.js
+++ src/app/containers/nbn-explained/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Page from '../../components/Page';
 import NbnConcept from './NbnConcept';
 import NbnTypes from './NbnTypes';
+import Install from './Install';
 import Pick from './Pick';
 import NbnHelp from './NbnHelp';
 import ImpInformation from './ImpInformation';
@@ -12,6 +13,7 @@ export default () => (
   <Page id="nbn-explained">
     <NbnConcept />
     <NbnTypes />
+    <Install />
     <Pick />
     <NbnHelp />
     <ImpInformation />
