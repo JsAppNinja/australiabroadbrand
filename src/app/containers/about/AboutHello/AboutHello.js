7593d7120c4605ccc46a1b98b84e3394f6ebb5c2
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-18 00:27:22 +0000

    Create AboutHello Section

---
+++ src/app/containers/about/AboutHello/AboutHello.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import EXPLAIN_IMG3 from '../../../assets/nbn-concept-img1.png';
+import EXPLAIN_IMG4 from '../../../assets/svgs/nbn-concept2.svg';
+import EXPLAIN_IMG5 from '../../../assets/logo.png';
+import './style.scss';
+
+const NbnConcept = () => (
+  <div className="about__hello">
+    <div className="about__hello-txt">
+      <div className="about__hello-txt__title">
+        Hello, we’re Australia Broadband
+      </div>
+      <div className="about__hello-txt__content">
+        At the heart of it, we believe in simplicity. A better way to do telco.
+        <br />
+        <br />
+        We’d rather you stay with us because you want to, not because there’s a
+        nasty contract forcing you to.
+        <br />
+        <br />
+        It’s why we work our socks off every day to keep you. We’re also big on
+        that caring, sharing thing. That’s why we’ll reward you when you Share
+        the Love with your friends.
+        <br />
+        <br />
+        We’ve helped tens of thousands of Australians connect to the nbn™ and
+        we'd love to help you too!
+      </div>
+    </div>
+    <div className="about__hello-img">
+      <div className="explain-img__topleft">
+        <div className="explain-img__topleft-line" />
+        History of nbn™
+        <br />
+        and the journey
+        <br />
+        ahead
+      </div>
+      <div className="explain-img__topright">
+        <img
+          src={EXPLAIN_IMG4}
+          alt="lprem"
+          style={{
+            width: '40px',
+          }}
+        />
+      </div>
+      <div className="explain-img__bottomleft" />
+      <div
+        className="explain-img__bottomright"
+        style={{
+          width: '420px',
+          height: '290px',
+          backgroundImage: 'url(' + EXPLAIN_IMG3 + ')',
+        }}
+      >
+        <img src={EXPLAIN_IMG5} alt="lprem" />
+        <div className="explain-img-text">
+          <div className="explain-img_button">&#9658;</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default NbnConcept;
