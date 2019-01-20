08a8146f59a97f3d3290c9742cbb93f81c78f141
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-20 01:37:37 +0000

    spread page added

---
+++ src/app/containers/spread/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from '../../components/Page';
+import ImpInformation from './ImpInformation';
+import Footer from '../footer';
+import './style.scss';
+
+export default () => (
+  <Page id="spread" title="Spread">
+    <ImpInformation />
+    <Footer />
+  </Page>
+);
