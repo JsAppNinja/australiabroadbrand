04892387be77d142d2e1d5c214be114bebd9d2ec
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-22 23:37:15 +0000

    Sharelove section assets apply

--- src/app/containers/spread/sharelove/sharelove.js
+++ src/app/containers/spread/sharelove/sharelove.js
@@ -1,6 +1,19 @@
 import React from 'react';
-
+import CheckButton from '../../../components/CheckButton';
+import ShareLove from '../../../assets/spread-sharelove.png';
 import './style.scss';
+
+const customStyle = {
+  width: '180px',
+  height: '38px',
+  backgroundColor: '#67cebb',
+  color: '#ffffff',
+  borderRadius: '22.5px',
+  fontSize: '16px',
+  fontWeight: 'bold',
+};
+
+const btnTitle = 'Start sharing';
 
 const sharelove = () => (
   <div className="sharelove">
