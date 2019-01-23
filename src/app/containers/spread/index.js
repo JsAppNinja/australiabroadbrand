9db52b56853b76ab710ca0aa6ee7f1906b9eb373
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-23 01:07:51 +0000

    integrate spread whyshare

--- src/app/containers/spread/index.js
+++ src/app/containers/spread/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Page from '../../components/Page';
 import Sharelove from './Sharelove';
 import Startshare from './Startshare';
+import Whyshare from './Whyshare';
 import ImpInformation from './ImpInformation';
 import Footer from '../footer';
 import './style.scss';
@@ -10,6 +11,7 @@ export default () => (
   <Page id="spread" title="Spread">
     <Sharelove />
     <Startshare />
+    <Whyshare />
     <ImpInformation />
     <Footer />
   </Page>
