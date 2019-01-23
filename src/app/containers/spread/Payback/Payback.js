4774a8b88cfd4f95289e748da8b905a3f04a0b9d
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-23 02:25:04 +0000

    Spread Payback section ui build

--- src/app/containers/spread/Payback/Payback.js
+++ src/app/containers/spread/Payback/Payback.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ArrowDown from '../../../assets/svgs/angle-down-solid.svg';
+import PAYBACK_IMG from '../../../assets/svgs/spread-payback1.svg';
+import PAYBACK_FAV from '../../../assets/svgs/spread-payback2.svg';
 import './style.scss';
 
-const ImpInformation = () => (
-  <div className="imp-information">
-    Important information
-    <div className="imp-information__img">
-      <img src={ArrowDown} alt="lorem" height="30" width="30" />
+const Payback = () => (
+  <div className="payback">
+    <div className="payback-img">
+      <img src={PAYBACK_IMG} alt="payback" />
+    </div>
+    <div className="payback-faq">
+      <div className="payback-faq__title">FAQs </div>
+      <div className="payback-faq__img">
+        <img src={PAYBACK_FAV} alt="payback" />
+      </div>
+      <div className="payback-faq__content">
+        A gift card is similar to gift vouchers or store cards. Each gift card
+        can also be split into any combination of the gift cards and vouchers
+        above, up to the total value of $50 for each friend you bring on board.
+      </div>
+      <div className="payback-faq__learn">Learn more</div>
     </div>
   </div>
 );
 
-export default ImpInformation;
+export default Payback;
--- src/app/containers/spread/Payback/index.js
+++ src/app/containers/spread/Payback/index.js
@@ -1,3 +1,3 @@
-import ImpInformation from './ImpInformation';
+import Payback from './Payback';
 
-export default ImpInformation;
+export default Payback;
