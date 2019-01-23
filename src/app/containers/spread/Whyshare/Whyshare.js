42346c6b48629c97c5e21ef551afca4b7b7236f4
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-23 02:07:44 +0000

    spread whyshare style update

--- src/app/containers/spread/Whyshare/Whyshare.js
+++ src/app/containers/spread/Whyshare/Whyshare.js
@@ -25,7 +25,9 @@ const btnTitle = 'Start sharing';
 const Whyshare = () => (
   <div className="whyshare">
     <div className="whyshare-left">
-      <img src={Whyshare1} alt="share" />
+      <div className="whyshare-left__img">
+        <img src={Whyshare1} alt="share" />
+      </div>
       <div className="whyshare-left__content">
         Share your personalised link on social media and invite your friends to
         join Australia Broadband.
@@ -38,27 +40,31 @@ const Whyshare = () => (
           “I refer around 3 people a month and get $150 a month to spend. Great
           for buying tickets for the footy, stuff from Myer and sometimes on
           groceries at Woolies. It is a great way to save and earn some dollars
-          on the side.” John
+          on the side.”
+          <br />
+          <b>John</b>
         </div>
         <div className="whyshare-content__down">
           “I referred my brother to Australia Broadband and received a $50 gift
-          card that I donated to the RSPCA.” Sarah
+          card that I donated to the RSPCA.”
+          <br />
+          <b>Sarah</b>
         </div>
       </div>
       <div className="whyshare-lefttop">
         <img src={Whyshare2} alt="share" />
       </div>
       <div className="whyshare-lefttop1">
-        <img src={Whyshare3} alt="share" />
-      </div>
-      <div className="whyshare-leftdown">
         <img src={Whyshare4} alt="share" />
       </div>
-      <div className="whyshare-righttop">
+      <div className="whyshare-leftdown">
         <img src={Whyshare5} alt="share" />
       </div>
+      <div className="whyshare-righttop">
+        <img src={Whyshare3} alt="share" />
+      </div>
       <div className="whyshare-rightdown">
-        <img src={Whyshare6} alt="share" />
+        <img src={Whyshare4} alt="share" />
       </div>
     </div>
   </div>
