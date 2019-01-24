c6fa89e9b8454cc7d0cee7e5d69048558bad7b30
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-01-24 10:38:57 +0000

    Spread Page style update

--- src/app/containers/spread/index.js
+++ src/app/containers/spread/index.js
@@ -6,15 +6,28 @@ import Whyshare from './Whyshare';
 import Payback from './Payback';
 import ImpInformation from './ImpInformation';
 import Footer from '../footer';
+import Container from '../../components/Container';
 import './style.scss';
 
 export default () => (
   <Page id="spread" title="Spread">
-    <Sharelove />
-    <Startshare />
-    <Whyshare />
-    <Payback />
-    <ImpInformation />
-    <Footer />
+    <Container bgColor="#f6f9ff">
+      <Sharelove />
+    </Container>
+    <Container>
+      <Startshare />
+    </Container>
+    <Container bgColor="#f6f9ff">
+      <Whyshare />
+    </Container>
+    <Container>
+      <Payback />
+    </Container>
+    <Container bgColor="#e7edfa">
+      <ImpInformation />
+    </Container>
+    <Container>
+      <Footer />
+    </Container>
   </Page>
 );
