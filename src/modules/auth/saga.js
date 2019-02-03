aafbd11e3e7180155b2a530d1c261beb844e424a
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-02-03 14:25:39 +0000

    Merge update

--- src/modules/auth/saga.js
+++ src/modules/auth/saga.js
@@ -1,5 +1,4 @@
 import { fork, call, put, takeLatest } from 'redux-saga/effects';
-//import { fetchBook } from 'api/BookApi';
 import { getAuthicationAction } from './actions';
 import { AUTHENTICATE, SET_CURRENT_USER } from './actions';
 
@@ -7,13 +6,4 @@ export default function* authSaga() {
   yield takeLatest(SET_CURRENT_USER, userLogInSaga);
 }
 
-function* userLogInSaga(action) {
-  try {
-    // Saga part call
-    // const bookUuid = action.payload;
-    // const response = yield call(fetchBook, bookUuid);
-    yield put(getAuthicationAction(true));
-  } catch (error) {
-    // yield put(receiveMessageAction(error));
-  }
-}
+function* userLogInSaga(action) {}
